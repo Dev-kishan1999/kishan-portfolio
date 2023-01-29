61c39f50b7e2e5e841630adc5c98dc3fa2139287
Kishan Savaliya <kishansavaliya@Kishans-MacBook-Air.local>
2023-01-29 14:54:00 -0500

    optimization

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.css";
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 const Navbar = ({ title }) => {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <nav className={styles.navbar}>
-      <h1 style={{ color: "#1d3557" }}>{title}</h1>
       <div>
-        <Link to="/aboutme" className={styles.navLinks}>
-          About Me
-        </Link>
-        <Link to="/work" className={styles.navLinks}>
-          Work
-        </Link>
-        <Link to="/projects" className={styles.navLinks}>
-          Projects
-        </Link>
+        <h1 style={{ color: "#1d3557" }}>{title}</h1>
+        <div
+          className={
+            clicked ? styles.navbarContainerActive : styles.navbarContainer
+          }
+        >
+          <Link to="/aboutme" className={styles.navLinks}>
+            About Me
+          </Link>
+          <Link to="/work" className={styles.navLinks}>
+            Work
+          </Link>
+          <Link to="/projects" className={styles.navLinks}>
+            Projects
+          </Link>
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          setClicked(!clicked);
+        }}
+        className={styles.mobile}
+      >
+        {clicked ? <AiOutlineClose /> : <GiHamburgerMenu />}
       </div>
     </nav>
   );
